28a2e6df8d05012cf34291042daa882afe4f8e6e
ankhbayar0415 <ane171@aubg.edu>
2022-04-03 08:30:04 +0300

    upload fixed v2

--- frontend/src/components/Upload.jsx
+++ frontend/src/components/Upload.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import PostCards from "./PostCards";
 import { ReactComponent as BackButton } from "../images/backButton.svg";
 
@@ -11,6 +11,7 @@ export default class Upload extends Component {
     this.state = {
       error: null,
       description: "",
+      posted: false,
       previewPost: {
         imgSrc: "",
         description: "Description",
@@ -22,28 +23,55 @@ export default class Upload extends Component {
     this.fileInput = React.createRef();
   }
   render() {
-    return (
-      <form className="uploadForm" onSubmit={this.handleSubmit}>
-        <h3>Upload an image so all you followers can see it!</h3>
-        <textarea
-          type="textarea"
-          name="description"
-          placeholder="Description"
-          value={this.state.description}
-          onChange={this.handleChange}
-        />
-        <input
-          type="file"
-          name="image"
-          ref={this.fileInput}
-          onChange={this.preview}
-        />
-        <input type="submit" className="btn btn-success" value="Upload" />
-
-        <h3>Preview:</h3>
-        <PostCards data={this.state.previewPost}></PostCards>
-      </form>
-    );
+    if (this.state.posted) {
+      return <Redirect to="/" />;
+    } else {
+      return (
+        <main>
+          <div className="container-upload">
+            <div className="upload-box">
+              <div className="upload-header">
+                <div className="upload-header-content">
+                  <Link to="/">
+                    <BackButton className="backButton" />
+                  </Link>
+                  Create New Post
+                  <a onClick={this.handleSubmit}>Share</a>
+                </div>
+              </div>
+              <div className="upload-content">
+                <div className="upload-pic">
+                  <div className="preview-image">
+                    <PostCards data={this.state.previewPost}></PostCards>
+                  </div>
+                  <input
+                    type="file"
+                    name="image"
+                    id="image"
+                    className="upload-icon"
+                    ref={this.fileInput}
+                    onChange={this.preview}
+                  />
+                  <label htmlFor="image">Choose a file</label>
+                </div>
+                <div className="upload-desc">
+                  <div className="upload-desc-header">Description</div>
+                  <textarea
+                    placeholder="Write a description..."
+                    name="description"
+                    rows="8"
+                    cols="50"
+                    className="upload-desc-desc"
+                    value={this.state.description}
+                    onChange={this.handleChange}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+      );
+    }
   }
   preview = (e) => {
     let self = this;
@@ -94,6 +122,9 @@ export default class Upload extends Component {
       if (res.ok) {
         res.text().then((text) => {
           console.log(text);
+          this.setState({
+            posted: true,
+          });
         });
       } else {
         res.text().then((text) => {
